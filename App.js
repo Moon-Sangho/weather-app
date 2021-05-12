9a4117c6cb9d320095ac691c18357ea52ab7fcca
Moon-Sangho <skym887@gmail.com>
2021-05-12 20:12:59 +0900

    feat: Getting the Weather Information

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Alert } from "react-native";
-import Loading from "./Loading";
+import axios from "axios";
 import * as Location from "expo-location";
-
+import Loading from "./Loading";
+import { WEATHER_API_KEY } from "./constants";
 class App extends React.Component {
   state = {
     isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
+    );
+    console.log(data);
   };
   getLocation = async () => {
     try {
       await Location.getForegroundPermissionsAsync();
       const {
         coords: { latitude, longitude },
-        // Send to API and get weather!
       } = await Location.getCurrentPositionAsync();
+
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (err) {
       Alert.alert("Can't find you.");
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Weather App Based on React Native
+
+## Usage
+
+```
+yarn install
+yarn start
+
+or
+
+npm install
+npm start
+```
+
+## Notice
+
+For execute this application, you have to create your `API KEY` from below web-site. <br>
+
+https://openweathermap.org/
+
+1. Sign-up, and Create your API KEY.
+2. create constants.js file in root directory.
+3. write `const WEATHER_API_KEY = 'your API KEY'`
